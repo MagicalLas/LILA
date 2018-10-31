9a5b77345378c3cac85c9a9d553468d874708958
Wonho ha <haho013039@gmail.com>
2018-10-31 11:07:25 +0900

    GET data test

--- src/main/java/las/wonho/lila/BaseController.java
+++ src/main/java/las/wonho/lila/BaseController.java
@@ -3,14 +3,15 @@ package las.wonho.lila;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BaseController {
     @RequestMapping(value = "/")
     @ResponseBody
-    String test() {
-        return "test";
+    String test(@RequestParam(value  = "name") String name) {
+        return name;
     }
     @RequestMapping(value = "/main")
     public String main(Model m) {
