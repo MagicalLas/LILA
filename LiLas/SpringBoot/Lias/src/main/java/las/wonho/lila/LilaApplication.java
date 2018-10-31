9d48e01d569733cb632720382740d13ad29aa623
Wonho ha <haho013039@gmail.com>
2018-10-31 13:04:44 +0900

    New Gradle Project

---
+++ LiLas/SpringBoot/Lias/src/main/java/las/wonho/lila/LilaApplication.java
@@ -0,0 +1,11 @@
+package las.wonho.lila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class LilaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LilaApplication.class, args);
+    }
+}
