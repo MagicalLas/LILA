d60e17fcb3e34c6a48043ad1c2eec351351c70c0
Wonho ha <haho013039@gmail.com>
2018-11-01 19:46:45 +0900

    Connect Mysql

--- LiLas/Express/routes/index.js
+++ LiLas/Express/routes/index.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '6644',
+    port     : '6644',
+    database : 'Lila',
+    insecureAuth:true
+});
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', {name:'Wonho'});
-});
-
-router.get('/login', function (req, res, next) {
-    res.send("Wonho");
 });
 
 
