077f82b628604d4eb17a8bfe3ea692756dba843e
Wonho ha <haho013039@gmail.com>
2018-11-01 21:13:08 +0900

    Fix HashFunction

--- LiLas/Express/Lila_tools/Hash.js
+++ LiLas/Express/Lila_tools/Hash.js
@@ -1,7 +1,7 @@
 var crypto = require('crypto');
-var shasum = crypto.createHash('sha1');
 
-export function makeHash(msg){
+module.exports.makeHash = function(msg){
+    var shasum = crypto.createHash('sha1');
     shasum.update(msg);
     return shasum.digest('hex');
-}
+};
