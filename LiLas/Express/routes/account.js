00dea7fd31284cb30c8dc47102219fed87daf11c
Wonho ha <haho013039@gmail.com>
2018-11-02 21:22:08 +0900

    Add account router

---
+++ LiLas/Express/routes/account.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+var Hash = require('../Lila_tools/Hash');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '6644',
+    port: '6644',
+    database: 'Lila',
+    insecureAuth: true
+});
+
+router.get('/group', (req, res) => {
+});
+
+module.exports = router;
