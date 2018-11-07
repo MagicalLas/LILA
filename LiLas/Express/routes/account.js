41f01c8ccdb2356ca76dc97334343171b0c33a67
Wonho ha <haho013039@gmail.com>
2018-11-07 15:58:41 +0900

    change host

--- LiLas/Express/routes/account.js
+++ LiLas/Express/routes/account.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var mysql = require('mysql');
 var Hash = require('../Lila_tools/Hash');
 var connection = mysql.createConnection({
-    host: 'localhost',
+    host: '45.76.222.210',
     user: 'root',
     password: '6644',
     port: '6644',
